019e454a036a86d43d7d50f2de842fb84ad95ca2
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-03-28 06:48:30 -0700

    add atatus

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 import App from './App';
+import * as atatus from "atatus-spa";
+
+atatus.config("4d9aadd413334e73bef14fa057292721").install();
 
 const root = document.getElementById("root");
 render(<App />, root);
+
+atatus.notify(new Error("Test Atatus Setup"));
