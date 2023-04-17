99a740d564fcd70877dcbe147a81f5e903d4ef79
MichaelHagler <67695612+MichaelHagler@users.noreply.github.com>
2023-04-17 07:03:19 -0700

    getting app verified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./nprogress.css";
 import React, { Component } from "react";
 import "./App.css";
-import { extractLocations, getEvents } from "./api";
+import { extractLocations, getEvents, checkToken, getAccessToken } from "./api";
 import EventList from "./EventList";
 import CitySearch from "./CitySearch";
 import NumberOfEvents from "./NumberOfEvents";
 import { WarningAlert } from "./Alert";
+import WelcomeScreen from "./WelcomeScreen";
 
 class App extends Component {
   state = {
@@ -13,6 +14,7 @@ class App extends Component {
     locations: [],
     numberOfEvents: 32,
     selectedLocation: "all",
+    showWelecomeScreen: undefined,
   };
 
   updateEvents = (location, inputValue) => {
@@ -45,23 +47,29 @@ class App extends Component {
       //warning to user if they are offline
       if (!navigator.onLine) {
         this.setState({
-          warningText: "You are offline, events pull from cache."
+          warningText: "You are offline, events pull from cache.",
         });
       } else {
         this.setState({
-          warningText: ""
+          warningText: "",
         });
       }
     }
   };
   async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        events = events.slice(0, this.state.numberOfEvents);
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
+    const accessToken = localStorage.getItem("access_token");
+    const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({ events, locations: extractLocations(events) });
+        }
+      });
+    }
   }
 
   componentWillUnmount() {
@@ -69,6 +77,8 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.showWelcomeScreen === undefined)
+      return <div className="App" />;
     return (
       <div className="App">
         <WarningAlert text={this.state.warningText} />
@@ -81,6 +91,12 @@ class App extends Component {
         <NumberOfEvents
           updateEvents={this.updateEvents}
           numberOfEvents={this.state.numberOfEvents}
+        />
+        <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
         />
       </div>
     );
